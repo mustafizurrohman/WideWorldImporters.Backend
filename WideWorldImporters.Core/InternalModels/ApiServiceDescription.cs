dc44cd505540a0a2d2d655acf41ff940d018b08d
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2019-06-17 22:01:06 +0200

    Restructuring of IServiceCollectionExtensionMethods

---
+++ WideWorldImporters.Core/InternalModels/ApiServiceDescription.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static WideWorldImporters.Core.Enumerations.ServiceLifetime;
+
+namespace WideWorldImporters.Core.InternalModels
+{
+
+    /// <summary>
+    /// Describes a service
+    /// </summary>
+    public class ApiServiceDescription
+    {
+
+        /// <summary>
+        /// Type
+        /// </summary>
+        public Type Implementation { get; set; }
+
+        /// <summary>
+        /// Type
+        /// </summary>
+        public Type Interface { get; set; }
+
+        /// <summary>
+        /// Describes the lifetime of the service
+        /// </summary>
+        public Lifetime Lifetime { get; set; }
+
+    }
+}
