c6e52cad4fc9128c46924b37a81df526b74af8f6
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2019-06-21 21:11:35 +0200

    gitflow-feature-stash: Logging

---
+++ WideWorldImporters.AuthenticationProvider/BaseClasses/BaseAuthenticationProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WideWorldImporters.AuthenticationProvider.BaseClasses
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class BaseAuthenticationProvider
+    {
+
+
+        public abstract Task Authenticate(HttpContext context);
+    }
+}
